6c52ad1f11f7dd41ff19b3fbc14235dee32d970f
Davis Adaptive RF Technology Lab <dart.ucdavis@gmail.com>
2015-08-27 00:53:42 -0700

    update python processing files

--- labs/lab6/doppler_wav.py
+++ labs/lab6/doppler_wav.py
@@ -1,51 +1,73 @@
 # -*- coding: utf-8 -*-
-#Doppler Radar, Read data from a WAV file
-#written by Meng Wei,  a summer exchange student (UCD GREAT Program, 2014) from Zhejiang University, China
+#Doppler speed calculation and display using data read from a WAV file
+# Originally written by Meng Wei,  a summer exchange student (UCD GREAT Program, 2013) from Zhejiang University, China
+# Modified by Xiaoguang "Leo" Liu (lxgliu@ucdavis.edu), Aug.2015
 
+import os
 import wave
 from struct import unpack
-from numpy.fft import ifft
+import numpy as np
+from numpy.fft import fft
 from math import log
 import matplotlib.pyplot as plt
 
-
-#read the raw data .wave file here
-wavefile = wave.open(r"/boot/Off of Newton Exit 17.wav", "rb")
-framerate = wavefile.getframerate()
-numframes = wavefile.getnframes()
-
-s=[]
-for i in range(numframes):
-    val = wavefile.readframes(1)
-    right = val[2:4]
-    u = unpack('h', right )[0]
-    s.append(u*(-1.0)/32768.0)
-
 #constants
 c= 3E8 #(m/s) speed of light
+fc=2590E6  #transmitting frequency of the radar
+Tp = 0.2 #(s) pulse time
 
-#radar parameters
-FS=framerate
-Tp = 0.250 #(s) pulse time
-N = Tp*FS  ## of samples per pulse
-fc=2590E6
+#read the raw data .wav file here
+#get path to the .wav file
+filename = os.getcwd() + '\\highway_doppler.wav'
 
-#creat doppler vs. time plot data set here
-sif=[]
-for ii in range(1,int(round(len(s)/N))):
-             sif.append(s[int((ii-1)*N):int(ii*N)])
-          
+#.wav file header information
+wavefile = wave.open(filename, "rb")
+
+#sampling rate
+FS = wavefile.getframerate()
+
+N = int(Tp*FS)  ## of samples per pulse
+
+numframes = wavefile.getnframes()
+#numframes = N*4
+
+#number of rows
+rows = int(numframes/N)
+
+#s stores the sampled data in the .wav file
+s=np.zeros([rows,N])
+
+for i in range(0,rows):
+    data = wavefile.readframes(N)
+    for j in range(0,N):
+        #get the right channel
+        right = data[4*j+2:4*j+4]
+        #.wav file store the sound level information in signed 16-bit integers stored in little-endian format
+        #The "struct" module provides functions to convert such information to python native formats, in this case, integers.
+        u = unpack('h', right)[0]            
+        #normalize the value to 1 and store them in a two dimensional array "s"        
+        s[i][j]=u/32768.0
+
 #subtract the average DC term here
-me=sum(s)/len(s)
-sif = [[x - me for x in y]for y in sif]
+mean=np.mean(s)
+s = [[x - mean for x in y]for y in s]
 zpad = int(8*N/2)
 
+#apply a Hamming window to reduce fft sidelobes
+for i in range(0,rows):
+    s[i]=np.multiply(s[i],np.hamming(N))
+    
 #doppler vs. time plot
-v=ifft(sif)  #now v is a [[]]
-vvv=[[20*log(abs(x),10) for x in y]for y in v]
-S=[x[:int(len(vvv[0])/2)] for x in vvv]
-m=max(max(S))
+v=fft(s)  #now v is a [[]]
 
+#get magnitude
+v=[[20*log(abs(x),10) for x in y]for y in v]
+
+#only the first half in each row contains unique information
+S=[x[:int(len(v[0])/2)] for x in v]
+m=np.max(S)
+
+#normalized with respect to its maximum value so that maximum is 0
 grid=[[x-m for x in y] for y in S]
 
 #calculate velocity
@@ -54,6 +76,7 @@ lambdaa=c/fc;
 velocity = delta_f*lambdaa/2;
 #calculate time
 time = Tp*len(v)
+plt.figure()
 plt.imshow(grid, extent=[0,velocity,0,time],aspect='auto')
 plt.xlim(0,30)
 plt.colorbar()
